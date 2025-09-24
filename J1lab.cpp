754c3999089699b8dc8eabb4c6ce174335c1a5aa
Yera-Zhumash <shimotsukikillita@gmail.com>
2025-09-25 02:09:15 +0500

    J1lab.cpp

--- J1lab.cpp
+++ J1lab.cpp
@@ -5,17 +5,17 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    deque<long long> dq;      // наша двусторонняя очередь
+    deque<long long> dq;     
     char cmd;
     long long x;
 
-    while (cin >> cmd) {      // читаем по одному символу команды
-        if (cmd == '!') break;                 // конец
-        else if (cmd == '+') { cin >> x; dq.push_front(x); }  // в начало
-        else if (cmd == '-') { cin >> x; dq.push_back(x); }   // в конец
+    while (cin >> cmd) {      
+        if (cmd == '!') break;                 
+        else if (cmd == '+') { cin >> x; dq.push_front(x); }  
+        else if (cmd == '-') { cin >> x; dq.push_back(x); }   
         else if (cmd == '*') {
-            if (dq.empty()) cout << "error\n";               // пусто
-            else if (dq.size() == 1) {                       // одна карта
+            if (dq.empty()) cout << "error\n";             
+            else if (dq.size() == 1) {                       
                 cout << dq.front() * 2 << "\n";
                 dq.pop_front();
             } else {
@@ -28,3 +28,4 @@ int main() {
     }
     return 0;
 }
+
